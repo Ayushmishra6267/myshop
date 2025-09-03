7f46a6491b214c495a17311ea7bf52bc53fad0b7
Ayushmishra <nitinmishra6267@gmail.com>
2025-09-03 09:11:09 +0530

    syntax err

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
@@ -9,29 +9,26 @@ import headphone from "./assets/headphone.png";
 import Products from "./components/Products/Products";
 import watch from "./assets/smartwatch2-removebg-preview.png";
 
-
 const BannerData = {
-  discount:"30% OFF",
+  discount: "30% OFF",
   title: "Fine Smile",
   date: "10 August to 10 September",
   image: headphone,
-  title2:"Air Solo Bass",
-  title3:"Mid Year Sale",
-  title4:"akjxbqsijxuixhiuxhiuxhi",
-  bgColor:"#410708ff",
-
+  title2: "Air Solo Bass",
+  title3: "Mid Year Sale",
+  title4: "akjxbqsijxuixhiuxhiuxhi",
+  bgColor: "#410708ff",
 };
 
 const BannerData2 = {
-  discount:"70% OFF",
+  discount: "70% OFF",
   title: "Fine Smile",
   date: "10 August to 10 September",
-  image: watch ,
-  title2:"Watch Supermacy",
-  title3:"Mid Year Sale",
-  title4:"Press shop now and get 70% discount",
-  bgColor:"#2b5c51ff",
-
+  image: watch,
+  title2: "Watch Supermacy",
+  title3: "Mid Year Sale",
+  title4: "Press shop now and get 70% discount",
+  bgColor: "#2b5c51ff",
 };
 const App = () => {
   return (
